69e63e6559ed54a550cead0be20036e34bd33bf9
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-10-13 22:21:53 +0100

    test.sh tries all testcases

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-./gocompiler.out < testcases/$1.dgo | diff testcases/$1.out -
+for input in testcases/*.dgo; do
+    filename=`echo $input | cut -d "." -f1`
+    out=$(./gocompiler.out -l < $filename.dgo | diff $filename.out -)
+    if [ $? -eq 0 ]; then
+        echo "✅ PASSED $filename.dgo"
+    else
+        echo "❌ FAILED $filename.dgo"
+    fi
+done
