f8f4abc943b3335f292a5350378dad4f6c18dabc
ZeTheNerd <jgon.10113@gmail.com>
2021-11-15 16:55:42 +0000

    added func_dec, func_header, param_dec and func_body structs

--- structs.h
+++ structs.h
@@ -1,8 +1,31 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-typedef struct {
+typedef struct _fun_body
+{
+
+} func_body;
+
+// function parameter declaration node
+typedef struct _param_dec
+{
+    v_type typespec;
     char* id;
+    struct _param_dec* next;
+} param_dec;
+
+// function header node
+typedef struct _func_header
+{
+    char* id;
+    v_type typespec;
+    param_dec* param;
+} func_header;
+
+// function declaration node
+typedef struct _func_dec{
+    func_header* f_header;
+    func_body* f_body;
 } func_dec;
 
 typedef enum {v_int, v_float, v_bool, v_string} v_type;
