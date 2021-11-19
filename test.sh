ede4dce83c7482224c785b5f890c2462cb23cc68
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-11-19 09:58:25 +0000

    Fixed test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 make # for making sure that the compiler is updated
 echo "Testcases"
 echo "---------------------------------------------"
-if [ $1 -eq "-1" ]; then
+if [ "$1" = "-l" ]; then
     path=testcases/meta1
+    flag="-l"
 else
-    path=testcases
+    path=testcases/meta2
+    flag="-t"
 fi
 
 for input in $path/*.dgo; do
     filename=`echo $input | cut -d "." -f1`
-    out=$(./gocompiler.out -l < $filename.dgo | diff $filename.out -)
+    out=$(./gocompiler.out $flag < $filename.dgo | diff $filename.out -)
     if [ $? -eq 0 ]; then
         echo "✅ PASSED $filename.dgo"
     else
