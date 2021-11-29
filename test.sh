659c404258c0c1bfc691358f256be58e83e41db6
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-11-29 14:38:15 +0000

    fixed segfault

--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
-make # for making sure that the compiler is updated
-echo "Testcases"
-echo "---------------------------------------------"
 if [ "$1" = "-l" ]; then
     path=meta1
     flag="$1"
 elif [ "$1" = "-t" ]; then
     path=meta2
     flag="$1"
-else
+elif [ "$1" = "-s" ]; then
     path=meta3
     flag="-s"
+else 
+    echo "No flag given!"
+    exit
 fi
+
+make # for making sure that the compiler is updated
+echo "Testcases"
+echo "---------------------------------------------"
 
 for input in $path/*.dgo; do
     filename=`echo $input | cut -d "." -f1`
