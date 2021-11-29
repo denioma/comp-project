f6ed7148a905258bd5b290b4240572c2c94a939f
ZeTheNerd <jgon.10113@gmail.com>
2021-11-29 14:43:26 +0000

    moved symtab files to src

--- src/symtab.h
+++ src/symtab.h
@@ -6,6 +6,7 @@ typedef enum { t_int, t_float32, t_bool, t_string } type;
 typedef struct _symtab {
     char* id;
     type type;
+    struct _symtab* next;
 } symtab;
 
 symtab* insert_el(symtab* tab, char* id, type type);
