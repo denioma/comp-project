52b79dd8c2a90718cf7443cd8602ea3c0ef52b98
ZeTheNerd <jgon.10113@gmail.com>
2021-11-29 15:57:47 +0000

    implemented symbol tables

--- src/symtab.c
+++ src/symtab.c
@@ -1,1 +1,52 @@
 #include "symtab.h"
+#include "string.h"
+
+// insert a new symbol in symbol table if it is not there yet
+symtab* insert_el(symtab** tab, char* id, t_type type, char is_func, f_params* params, char is_param){
+    // alocate space for new symbol
+    symtab* newSym = (symtab*)malloc(sizeof(symtab*));
+    symtab* aux;
+    symtab* prev;
+    // populate symbol fields
+    newSym->id = strdup(id);
+    newSym->type = type;
+    newSym->next = NULL;
+    newSym->is_func = is_func;
+    if (is_func){
+        newSym->params = params;
+    }
+    newSym->is_param = is_param;
+
+    if (*tab){
+        for (aux = *tab; aux; aux = aux->next){
+            if (strcmp(aux->id, id) == 0){
+                // if the symbol was already declared in tab, free space
+                free(newSym->id);
+                if (is_func) //invoque destroy f_params function;
+                free(newSym);
+                return NULL;
+            }
+            // save reference to previous symbol
+            prev = aux;
+        }
+        // if the symbol wasn't yet declared, added to the tail of tab
+        prev->next = newSym;
+    }
+    else{
+        // if the table doesn't exist, the new symbol will be the table head
+        *tab = newSym;
+    }
+
+    return newSym;
+}
+
+// search for symbol in the given symbol table
+symtab* search_el(symtab* tab, char* id){
+    symtab* aux;
+    for(aux = tab; aux; aux = aux->next){
+        if (strcmp(aux->id, id) == 0){
+            return aux;
+        }
+    }
+    return NULL;
+}
--- src/symtab.h
+++ src/symtab.h
@@ -1,15 +1,23 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
-typedef enum { t_int, t_float32, t_bool, t_string } type;
+typedef enum { t_int, t_float32, t_bool, t_string, t_void } t_type;
+
+typedef struct _f_params {
+    t_type type;
+    struct _f_params* next;
+} f_params;
 
 typedef struct _symtab {
     char* id;
-    type type;
+    t_type type;
+    char is_func;
+    f_params* params;
+    char is_param;
     struct _symtab* next;
 } symtab;
 
-symtab* insert_el(symtab* tab, char* id, type type);
+symtab* insert_el(symtab** tab, char* id, t_type type, char is_func, f_params* params, char is_param);
 symtab* search_el(symtab* tab, char* id);
 void show_table(symtab* tab);
 
