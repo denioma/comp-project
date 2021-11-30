e096052bc6f1d2365defc6934a22f248ae7ae8c2
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2021-11-30 19:46:56 +0000

    Local table headers now include param types

--- src/semantic.c
+++ src/semantic.c
@@ -18,7 +18,7 @@ t_type convert_v_type(v_type type) {
         return t_string;
         break;
     case v_void:
-        return t_void; 
+        return t_void;
         break;
     }
 }
@@ -27,7 +27,7 @@ int check_bool_var(symtab** tab, var_dec* var) {
     if (insert_el(tab, var->tkn->value, t_bool, 0, 0, 0))
         return 0;
 
-    printf("Line %d, col %d: Symbol %s already defined\n", 
+    printf("Line %d, col %d: Symbol %s already defined\n",
         var->tkn->line, var->tkn->col, var->tkn->value);
     return 1;
 }
@@ -36,7 +36,7 @@ int check_float_var(symtab** tab, var_dec* var) {
     if (insert_el(tab, var->tkn->value, t_float32, 0, 0, 0))
         return 0;
 
-    printf("Line %d, col: %d: Symbol %s already defined\n", 
+    printf("Line %d, col: %d: Symbol %s already defined\n",
         var->tkn->line, var->tkn->col, var->tkn->value);
     return 1;
 }
@@ -45,7 +45,7 @@ int check_int_var(symtab** tab, var_dec* var) {
     if (insert_el(tab, var->tkn->value, t_int, 0, 0, 0))
         return 0;
 
-    printf("Line %d, col %d: Symbol %s already defined\n", 
+    printf("Line %d, col %d: Symbol %s already defined\n",
         var->tkn->line, var->tkn->col, var->tkn->value);
     return 1;
 }
@@ -54,25 +54,25 @@ int check_string_var(symtab** tab, var_dec* var) {
     if (insert_el(tab, var->tkn->value, t_string, 0, 0, 0))
         return 0;
 
-    printf("Line %d, col %d: Symbol %s already defined\n", 
+    printf("Line %d, col %d: Symbol %s already defined\n",
         var->tkn->line, var->tkn->col, var->tkn->value);
     return 1;
 }
 
 
 int check_var(symtab** tab, var_dec* var) {
-    switch(var->typespec) {
-        case v_bool:
-            return check_bool_var(tab, var);
-        case v_float:
-            return check_float_var(tab, var);
-        case v_int:
-            return check_int_var(tab, var);
-        case v_string:
-            return check_string_var(tab, var);
-        case v_void:
-        default:
-            return 0;
+    switch (var->typespec) {
+    case v_bool:
+        return check_bool_var(tab, var);
+    case v_float:
+        return check_float_var(tab, var);
+    case v_int:
+        return check_int_var(tab, var);
+    case v_string:
+        return check_string_var(tab, var);
+    case v_void:
+    default:
+        return 0;
     }
 }
 
@@ -90,7 +90,7 @@ f_params* check_params(param_dec* params) {
         faux->type = convert_v_type(aux->typespec);
         faux->next = NULL;
     }
-    
+
     return fparams;
 }
 
@@ -126,16 +126,30 @@ int semantic_check(symtab** tab, prog_node* program) {
     return errors;
 }
 
+extern void print_sym_type(t_type);
+
+void print_v_type(v_type type) { print_sym_type(convert_v_type(type)); }
+
+void print_func_table(symtab* global, func_dec* fun) {
+    func_header* header = fun->f_header;
+    printf("\n===== Function %s(", header->tkn->value);
+    // Print return types - parse params typespec
+    param_dec* aux = header->param;
+    for (; aux; aux = aux->next) {
+        if (aux != header->param) printf(",");
+        print_v_type(aux->typespec);
+    }
+    printf(") Symbol Table =====\n");
+    show_table(fun->localsym);
+}
+
 void show_tables(symtab* global, prog_node* program) {
     printf("===== Global Symbol Table =====\n");
     show_table(global);
     if (!program->dlist) return;
     dec_node* aux = program->dlist;
     for (; aux; aux = aux->next) {
-        if (aux->type == d_func) {
-            printf("\n===== Function %s() Symbol Table =====\n",
-                aux->dec.func->f_header->tkn->value);
-            show_table(aux->dec.func->localsym);
-        }
+        if (aux->type == d_func)
+            print_func_table(global, aux->dec.func);
     }
 }
